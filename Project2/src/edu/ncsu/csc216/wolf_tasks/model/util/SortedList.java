e0ebbf66e02055fc02616524e3e1c8a29a1b5eb2
Aadhir Sandeep <asandee@ncsu.edu>
2024-03-24 17:47:19 -0400

    [Skeleton] Implemented Skeleton for SwapList and SortedList

---
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SortedList.java
@@ -0,0 +1,38 @@
+package edu.ncsu.csc216.wolf_tasks.model.util;
+
+/**
+ * Linked List
+ * @param <E>
+ */
+public class SortedList<E> {
+	private int size;
+	
+	public SortedList() {
+		
+	}
+	
+	public void add(E obj) {
+		
+	}
+	
+	public E remove(int idx) {
+		return null;
+	}
+	
+	private void checkIndex(int idx) {
+		
+	}
+	
+	public boolean contains(E obj) {
+		return false;
+	}
+	
+	public E get(int idx) {
+		return null;
+	}
+	
+	public int size() {
+		return 0;
+	}
+
+}
---
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
@@ -0,0 +1,61 @@
+package edu.ncsu.csc216.wolf_tasks.model.util;
+
+/**
+ * Array based List that implements the ISwapListInterface
+ * @param <E>
+ */
+public class SwapList<E> {
+	public static final int INITIAL_CAPACITY = 10;
+	private  E[] list;
+	private int size;	
+	
+	
+	public SwapList() {
+		
+	}
+	
+	
+	public void add(E obj) {
+		
+	}
+	
+	private void checkCapacity(int size) {
+		
+	}
+	
+	public E remove(int idx) {
+		
+		return null;
+	}
+	
+	private void checkIndex(int idx) {
+		
+	}
+	
+	public void moveUp(int idx) {
+		
+	}
+	
+	public void moveDown(int idx) {
+		
+	}
+	
+	public void moveToFront(int idx) {
+		
+	}
+	
+	public void moveToBack(int idx) {
+		
+	}
+	
+	public E get(int idx) {
+		return null;
+	}
+	
+	public int size() {
+		
+		return 0;
+	}
+	
+	
+}
