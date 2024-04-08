1777aac45be078873202343fd0c304753da45522
Aadhir Sandeep <asandee@ncsu.edu>
2024-04-08 12:27:06 -0400

    Tested SwapList

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/util/SwapList.java
@@ -26,7 +26,7 @@ public class SwapList<E> implements ISwapList<E> {
 	* @param obj the object that is being added 
 	*/
 	public void add(E obj) {
-		System.err.println();
+		System.err.println(obj);
 		if(obj == null) {
 			throw new IllegalArgumentException();
 		}
