b68ecf459fc031cdff269537e54920ebd6b3be6e
Sagnik Saha <ssaha8@ncsu.edu>
2024-04-08 15:30:38 -0400

    [Debugging] Changed error message for editTaskList

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/notebook/Notebook.java
@@ -145,7 +145,7 @@ public class Notebook {
     */
 	public void editTaskList(String taskListName) {
 		if(currentTaskList == activeTaskList || taskListName.equalsIgnoreCase(ActiveTaskList.ACTIVE_TASKS_NAME)) {
-			throw new IllegalArgumentException("Invalid name."); 
+			throw new IllegalArgumentException("The Active Tasks list may not be edited."); 
 		}
 		for(int i = 0; i < taskLists.size(); i++) {
 			if(taskLists.get(i).getTaskListName().equalsIgnoreCase(taskListName)) {
